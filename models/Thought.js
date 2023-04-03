8bacb69206887b70eda482b74b89c62104fd3cd5
Brian Chiang <kyaerorgabootcamp@gmail.com>
2023-04-03 01:20:54 -0400

    made all models

---
+++ models/Thought.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const reactionSchema = require(`./Reaction`)
+
+const thoughtSchema = new mongoose.Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: timestamp => dateFormat(timestamp)
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [reactionSchema]
+})
+
+thoughtSchema.virtual(`reactionCount`).get(function() {
+    return this.reactions.length
+})
+
+function dateFormat(date) {
+    return new Date(date).toLocaleDateString()
+}
+
+const Thought = mongoose.model(`Thought`, thoughtSchema)
+
+module.exports = Thought
