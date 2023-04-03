ef82630e50b006e05c414bd94fa7e12457250473
Brian Chiang <kyaerorgabootcamp@gmail.com>
2023-04-03 03:01:16 -0400

    everything I could do

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,26 @@
 const mongoose = require('mongoose')
-const reactionSchema = require(`./Reaction`)
 
+
+const reactionSchema = new mongoose.Schema({
+    reactionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        default: () => new mongoose.Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: timestamp => dateFormat(timestamp)
+    }
+})
 const thoughtSchema = new mongoose.Schema({
     thoughtText: {
         type: String,
